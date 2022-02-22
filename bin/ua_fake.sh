6fa4e5e4abe8d7ed85250b5e4d103d8b874fa884
MasayukiFukada <m.fukada0813@gmail.com>
2022-02-22 23:31:53 +0900

    UAフェイクを微調整

--- bin/ua_fake.sh
+++ bin/ua_fake.sh
@@ -2,5 +2,5 @@
 
 
 
-google-chrome --user-agent="Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0) AppleWebKit/602.3.12 (KHTML, like Gecko) Chrome/69.1" --user-data-dir="/tmp"
+google-chrome-stable --user-agent="Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0) AppleWebKit/602.3.12 (KHTML, like Gecko) Chrome/69.1" --user-data-dir="/tmp"
 
